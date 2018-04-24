a230e0d5c2814b73468459e2e68af792fa93aec2
Canino Developments <ian.canino@gmail.com>
2018-04-24 17:39:11 -0400

    created welcome message and added input ability

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,18 @@
 def welcome
-  # puts out a welcome message here!
+  puts <<-'EOF'
+      _______.___________.    ___      .______         ____    __    ____  ___      .______          _______.        ___      .______    __
+     /       |           |   /   \     |   _  \        \   \  /  \  /   / /   \     |   _  \        /       |       /   \     |   _  \  |  |
+    |   (----`---|  |----`  /  ^  \    |  |_)  |        \   \/    \/   / /  ^  \    |  |_)  |      |   (----`      /  ^  \    |  |_)  | |  |
+     \   \       |  |      /  /_\  \   |      /          \            / /  /_\  \   |      /        \   \         /  /_\  \   |   ___/  |  |
+ .----)   |      |  |     /  _____  \  |  |\  \----.      \    /\    / /  _____  \  |  |\  \----.----)   |       /  _____  \  |  |      |  |
+ |_______/       |__|    /__/     \__\ | _| `._____|       \__/  \__/ /__/     \__\ | _| `._____|_______/       /__/     \__\ | _|      |__|
+
+ EOF
+# puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character"
+  gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
