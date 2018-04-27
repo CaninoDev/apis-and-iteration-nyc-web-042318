2f47fab3aca3a52e4d8d667d7c33af126c9ffbbf
Canino Developments <ian.canino@gmail.com>
2018-04-27 01:52:03 -0400

    completed basic assignment

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,33 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  nextpage = false
-
+   #make the web request
   film_roster = []
-  # Ignore the top level fluff from the hash.
-  characters_infobus = character_hash["results"]
-  characters_infobus.each do |character_info|
-    if character_info["name"].downcase == character
-      film_roster = character_info["films"]
+  nextpage = 'http://www.swapi.co/api/people/'
+  all_characters = RestClient.get(nextpage)
+  character_hash = JSON.parse(all_characters)
+  nextpage = character_hash['next']
+  while film_roster.length == 0 && !nextpage.nil?
+    all_characters = RestClient.get(nextpage)
+    character_hash = JSON.parse(all_characters)
+    nextpage = character_hash['next']
+    character_infobus = character_hash['results']
+    character_infobus.each do |character_info|
+      film_roster = character_info['films']
       break
     end
   end
 
   film_roster
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+    # iterate over the character hash to find the collection of `films` for the given
+    #   `character`
+    # collect those film API urls, make a web request to each URL to get the info
+    #  for that film
+    # return value of this method should be collection of info about each film.
+    #  i.e. an array of hashes in which each hash reps a given film
+    # this collection will be the argument given to `parse_character_movies`
+    #  and that method will do some nice presentation stuff: puts out a list
+    #  of movies by title. play around with puts out other info about a given film.
 end
 
 def parse_character_movies(films_hash)
@@ -52,8 +54,3 @@ def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
